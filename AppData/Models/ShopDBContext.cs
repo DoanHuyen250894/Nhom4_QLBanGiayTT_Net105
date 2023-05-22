3712b28277e263334c41e10aeb42f6b6f28b0198
DoanHuyen250894 <huyendtph24484@fpt.edu.vn>
2023-05-22 12:05:36 +0700

    Add project files.

---
+++ AppData/Models/ShopDBContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppData.Models
+{
+    public class ShopDBContext : DbContext
+    {
+        public ShopDBContext()
+        {
+
+        }
+        public ShopDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<BillDetails> BillDetails { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Coupon> Coupons { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<ShoesDetails> ShoesDetails { get; set; }
+        public DbSet<Size> Sizes { get; set; }
+        public DbSet<Sole> Soles { get; set; }
+        public DbSet<Style> Styles { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Voucher> Vouchers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-A9URI7S\SQLEXPRESS;Initial Catalog=ShoesNET105_DBFirst;Persist Security Info=True; User ID =sa; Password =123456");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
