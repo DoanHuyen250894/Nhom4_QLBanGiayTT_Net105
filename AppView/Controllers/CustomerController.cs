dd2435b76a1fcb5d3d8e839ef35fccda5879766c
mongbien <duyvtph24890@fpt.edu.vn>
2023-05-23 20:27:05 +0700

    duy_commit_23/05/2023-20:26

---
+++ AppView/Controllers/CustomerController.cs
@@ -0,0 +1,74 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace AppView.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly IAllRepositories<Customer> _repos;
+        private ShopDBContext _dbContext = new ShopDBContext();
+        private DbSet<Customer> _customer;
+        public CustomerController()
+        {
+            _customer = _dbContext.Customers;
+            AllRepositories<Customer> all = new AllRepositories<Customer>(_dbContext, _customer);
+            _repos = all;
+        }
+        public async Task<IActionResult> GetAllCustomer()
+        {
+            string apiUrl = "https://localhost:7036/api/Customer/get-customer";
+            var httpClient = new HttpClient(); // tạo ra để callApi
+            var response = await httpClient.GetAsync(apiUrl);// Lấy dữ liệu ra
+                                                             // Lấy dữ liệu Json trả về từ Api được call dạng string
+            string apiData = await response.Content.ReadAsStringAsync();
+            // Lấy kqua trả về từ API
+            // Đọc từ string Json vừa thu được sang List<T>
+            var customer = JsonConvert.DeserializeObject<List<Customer>>(apiData);
+            return View(customer);
+        }
+        public async Task<IActionResult> CreateCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer(Customer customer)
+        {
+            string apiUrl = $"https://localhost:7036/api/Customer/create-customer?UserName={customer.UserName}&Password={customer.Password}&Email={customer.Email}&Sex={customer.Sex}";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(apiUrl);
+            string apiData = await response.Content.ReadAsStringAsync();
+            // Cập nhật thông tin từ apiData vào đối tượng customer
+            var newCustomer = JsonConvert.DeserializeObject<Customer>(apiData);
+            _repos.AddItem(customer);
+            return RedirectToAction("GetAllCustomer");
+        }
+        [HttpGet]
+        public IActionResult EditCustomer(Guid id) // Khi ấn vào Create thì hiển thị View
+        {
+            // Lấy Product từ database dựa theo id truyền vào từ route
+            Customer customer = _repos.GetAll().FirstOrDefault(c => c.CumstomerID == id);
+            return View(customer);
+        }
+        public IActionResult EditCustomer(Customer customer) // Thực hiện việc Tạo mới
+        {
+            if (_repos.EditItem(customer))
+            {
+                return RedirectToAction("GetAllCustomer");
+            }
+            else return BadRequest();
+        }
+        public IActionResult DeleteCustomer(Guid id)
+        {
+            var cus = _repos.GetAll().First(c => c.CumstomerID == id);
+            if (_repos.RemoveItem(cus))
+            {
+                return RedirectToAction("GetAllCustomer");
+            }
+            else return Content("Error");
+        }
+    }
+}
---
+++ AppView/Controllers/EmployeeController.cs
@@ -0,0 +1,82 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace AppView.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IAllRepositories<Employee> _repos;
+        private ShopDBContext _dbContext = new ShopDBContext();
+        private DbSet<Employee> _customer;
+        public EmployeeController()
+        {
+            _customer = _dbContext.Employees;
+            AllRepositories<Employee> all = new AllRepositories<Employee>(_dbContext, _customer);
+            _repos = all;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployee() 
+        {
+            string apiUrl = "https://localhost:7036/api/Employee/get-employee";
+            var httpClient = new HttpClient(); // tạo ra để callApi
+            var response = await httpClient.GetAsync(apiUrl);// Lấy dữ liệu ra
+                                                             // Lấy dữ liệu Json trả về từ Api được call dạng string
+            string apiData = await response.Content.ReadAsStringAsync();
+            // Lấy kqua trả về từ API
+            // Đọc từ string Json vừa thu được sang List<T>
+            var employees = JsonConvert.DeserializeObject<List<Employee>>(apiData);
+            return View(employees);
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateEmployee()
+        {
+            using (ShopDBContext shopDBContext = new ShopDBContext())
+            {
+                var role = shopDBContext.Roles.ToList();
+                SelectList selectListRole = new SelectList(role, "RoleID", "RoleName");
+                ViewBag.RoleList = selectListRole;
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee(Employee employee)
+        {
+            if (_repos.AddItem(employee))
+            {
+                return RedirectToAction("GetAllEmployee");
+            }
+            else return BadRequest();
+        }
+        [HttpGet]
+        public IActionResult UpdateEmployee(Guid id)
+        {
+            Employee employee = _repos.GetAll().FirstOrDefault(c => c.EmployeeID == id);
+            return View(employee);
+        }
+        public IActionResult UpdateEmployee(Employee employee)
+        {
+            if (_repos.EditItem(employee))
+            {
+                return RedirectToAction("GetAllEmployee");
+            }
+            else return Content("Loi");
+        }
+        public async Task<IActionResult> DeleteEmployee(Guid id)
+        {
+            var employee = _repos.GetAll().First(c => c.EmployeeID == id);
+            if (_repos.RemoveItem(employee))
+            {
+                return RedirectToAction("GetAllEmployee");
+            }
+            else
+            {
+                return Content("Error");
+            }
+        }
+    }
+}
