6b73a69561473a782d1063e1c0c2826efca6a2c2
KieuAnh <anhhkph24928@fpt.edu.vn>
2023-05-24 09:29:49 +0700

    anhht9/29aaa

---
+++ AppView/Controllers/ColorController.cs
@@ -0,0 +1,77 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace AppView.Controllers
+{
+    public class ColorController : Controller
+    {
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        private readonly IAllRepositories<Color> _repos;
+        private ShopDBContext _dbContext = new ShopDBContext();
+        private DbSet<Color> _color;
+        public ColorController()
+        {
+            _color = _dbContext.Colors;
+            AllRepositories<Color> all = new AllRepositories<Color>(_dbContext, _color);
+            _repos = all;
+        }  
+       
+        public async Task<IActionResult> GetAllColor()
+        {
+
+
+            string apiUrl = "https://localhost:7036/api/Color/get-color";
+            var httpClient = new HttpClient(); // tạo ra để callApi
+            var response = await httpClient.GetAsync(apiUrl);// Lấy dữ liệu ra
+                                                             // Lấy dữ liệu Json trả về từ Api được call dạng string
+            string apiData = await response.Content.ReadAsStringAsync();
+            // Lấy kqua trả về từ API
+            // Đọc từ string Json vừa thu được sang List<T>
+            var color = JsonConvert.DeserializeObject<List<Color>>(apiData);
+            return View(color);
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateColor()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateColor(Color color)
+        {
+            _repos.AddItem(color);
+            return RedirectToAction("GetAllColor");
+        }
+        [HttpGet]
+        public IActionResult EditColor(Guid id) // Khi ấn vào Create thì hiển thị View
+        {
+            // Lấy Product từ database dựa theo id truyền vào từ route
+            Color color = _repos.GetAll().FirstOrDefault(c => c.ColorID == id);
+            return View(color);
+        }
+        public IActionResult EditColor(Color color) // Thực hiện việc Tạo mới
+        {
+            if (_repos.EditItem(color))
+            {
+                return RedirectToAction("GetAllColor");
+            }
+            else return BadRequest();
+        }
+        public IActionResult DeleteColor(Guid id)
+        {
+            var colo = _repos.GetAll().First(c => c.ColorID == id);
+            if (_repos.RemoveItem(colo))
+            {
+                return RedirectToAction("GetAllColor");
+            }
+            else return Content("Error");
+        }
+    }
+}
---
+++ AppView/Controllers/CouponController.cs
@@ -0,0 +1,72 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace AppView.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly IAllRepositories<Coupon> _repos;
+        private ShopDBContext _dbContext = new ShopDBContext();
+        private DbSet<Coupon> _coupon;
+        public CouponController()
+        {
+            _coupon = _dbContext.Coupons;
+            AllRepositories<Coupon> all = new AllRepositories<Coupon>(_dbContext, _coupon);
+            _repos = all;
+        }
+       
+
+        public async Task<IActionResult> GetAllCoupon()
+        {
+            string apiUrl = "https://localhost:7036/api/Coupon/get-coupon";
+            var httpClient = new HttpClient(); // tạo ra để callApi
+            var response = await httpClient.GetAsync(apiUrl);// Lấy dữ liệu ra
+                                                             // Lấy dữ liệu Json trả về từ Api được call dạng string
+            string apiData = await response.Content.ReadAsStringAsync();
+            // Lấy kqua trả về từ API
+            // Đọc từ string Json vừa thu được sang List<T>
+            var coupon = JsonConvert.DeserializeObject<List<Coupon>>(apiData);
+            return View(coupon);
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateCoupon()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCoupon(Coupon coupon)
+        {
+            _repos.AddItem(coupon);
+            return RedirectToAction("GetAllCoupon");
+        }
+
+        [HttpGet]
+        public IActionResult EditCoupon(Guid id) // Khi ấn vào Create thì hiển thị View
+        {
+            // Lấy Product từ database dựa theo id truyền vào từ route
+            Coupon coupon = _repos.GetAll().FirstOrDefault(c => c.CouponID == id);
+            return View(coupon);
+        }
+        public IActionResult EditCoupon(Coupon coupon) // Thực hiện việc Tạo mới
+        {
+            if (_repos.EditItem(coupon))
+            {
+                return RedirectToAction("GetAllCoupon");
+            }
+            else return BadRequest();
+        }
+        public IActionResult DeleteCoupon(Guid id)
+        {
+            var coup = _repos.GetAll().First(c => c.CouponID == id);
+            if (_repos.RemoveItem(coup))
+            {
+                return RedirectToAction("GetAllCoupon");
+            }
+            else return Content("Error");
+        }
+    }
+}
