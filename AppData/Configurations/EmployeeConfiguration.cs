c655df7dc2b6e51a4acdada82ceef526fc833303
DoanHuyen250894 <huyendtph24484@fpt.edu.vn>
2023-06-09 12:14:41 +0700

    Okkkkeehhhefgf

--- AppData/Configurations/EmployeeConfiguration.cs
+++ AppData/Configurations/EmployeeConfiguration.cs
@@ -20,6 +20,7 @@ namespace AppData.Configurations
             builder.Property(c => c.Password).HasColumnType("nvarchar(100)");
             builder.Property(c => c.Email).HasColumnType("nvarchar(300)");
             builder.Property(c => c.Sex).HasColumnType("int");
+            builder.Property(c => c.ResetPassword).HasColumnType("nvarchar(50)");
             builder.Property(c => c.PhoneNumber).HasColumnType("nvarchar(50)");
             builder.Property(c => c.Status).HasColumnType("int");
             builder.HasOne(c => c.Role).WithMany(c => c.Employees).HasForeignKey(c => c.RoleID);
