a271b815187082e8b17b9f5b6a3fcb4e4cdd3e95
khanhlv <khanh.le1107@gmail.com>
2023-06-13 21:43:48 +0700

    Hoàn thành lọc sản phẩm (13/06 21:43)

--- AppView/Controllers/HomeController.cs
+++ AppView/Controllers/HomeController.cs
@@ -87,280 +87,122 @@ namespace AppView.Controllers
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
 
-		public IActionResult SexFilter()
+		public IActionResult Filters()
 		{
 			return View("ListProduct");
 		}
 
 		[HttpPost]
-		public IActionResult SexFilter(string[] genders)
+		public IActionResult Filters(string[] genders, string[] minPrice, string[] brands, string[] styles, string[] colors)
 		{
-			if (genders != null && genders.Length > 0)
+			if (genders != null && genders.Length > 0
+				|| minPrice != null && minPrice.Length > 0
+				|| brands != null && brands.Length > 0
+				|| styles != null && styles.Length > 0
+				|| colors != null && colors.Length > 0)
 			{
 				var combinedShoesList = new List<ShoesDetails>();
 				var comparingShoesList = _shoesDT.GetAllShoesDetails();
+
 				foreach (var shoes in comparingShoesList)
 				{
-					//kiểm tra trong db có thg image nào đã chứa thằng ShoesDetails tương ứng chưa, nếu tồn tại rồi gán cho thuộc tính imageUrl giá trị của link ảnh đó
+					// Check if the ShoesDetails has an associated image in the database
 					var firstImage = _image.GetAllImages().FirstOrDefault(c => c.ShoesDetailsID == shoes.ShoesDetailsId);
 					if (firstImage != null)
 					{
 						shoes.ImageUrl = firstImage.Image1;
 					}
+
+					// Check if the ShoesDetails has an associated product in the database
 					var nameProduct = _product.GetAllProducts().FirstOrDefault(c => c.ProductID == shoes.ProductID);
 					if (nameProduct != null)
 					{
 						ViewBag.NameSP = nameProduct.Name;
 					}
 				}
+
+				// Filter giới tính
 				if (genders.Contains("male"))
 				{
-					var sizesList = _size.GetAllSizes().Where(x => x.Status == 1);
+					var sizesList = _size.GetAllSizes().Where(x => x.Status == 1).ToList();
 					List<ShoesDetails> maleShoesList = comparingShoesList.Where(shoes => sizesList.Any(size => size.SizeID == shoes.SizeID)).ToList();
-					combinedShoesList.AddRange(maleShoesList);
+					AddRangeIfNotExist(combinedShoesList, maleShoesList);
 				}
 
 				if (genders.Contains("female"))
 				{
-					var sizesList = _size.GetAllSizes().Where(x => x.Status == 2);
-					List<ShoesDetails> maleShoesList = comparingShoesList.Where(shoes => sizesList.Any(size => size.SizeID == shoes.SizeID)).ToList();
-					combinedShoesList.AddRange(maleShoesList);
+					var sizesList = _size.GetAllSizes().Where(x => x.Status == 2).ToList();
+					List<ShoesDetails> femaleShoesList = comparingShoesList.Where(shoes => sizesList.Any(size => size.SizeID == shoes.SizeID)).ToList();
+					AddRangeIfNotExist(combinedShoesList, femaleShoesList);
 				}
 
 				if (genders.Contains("unisex"))
 				{
-					var sizesList = _size.GetAllSizes().Where(x => x.Status == 0);
-					List<ShoesDetails> maleShoesList = comparingShoesList.Where(shoes => sizesList.Any(size => size.SizeID == shoes.SizeID)).ToList();
-					combinedShoesList.AddRange(maleShoesList);
+					var sizesList = _size.GetAllSizes().Where(x => x.Status == 0).ToList();
+					List<ShoesDetails> unisexShoesList = comparingShoesList.Where(shoes => sizesList.Any(size => size.SizeID == shoes.SizeID)).ToList();
+					AddRangeIfNotExist(combinedShoesList, unisexShoesList);
 				}
-				ViewBag.shoesList = combinedShoesList;
-				return View("ListProduct");
-			}
-			else
-			{
-				return RedirectToAction("ListProduct");
-			}
-		}
 
-		public IActionResult SizeFilter()
-		{
-			return View("ListProduct");
-		}
-
-		[HttpPost]
-		public IActionResult SizeFilter(string[] sizes)
-		{
-			if (sizes != null && sizes.Length > 0)
-			{
-				var combinedShoesList = new List<ShoesDetails>();
-				var comparingShoesList = _shoesDT.GetAllShoesDetails();
-				foreach (var shoes in comparingShoesList)
+				//Filter khoảng giá
+				foreach (var price in minPrice)
 				{
-					//kiểm tra trong db có thg image nào đã chứa thằng ShoesDetails tương ứng chưa, nếu tồn tại rồi gán cho thuộc tính imageUrl giá trị của link ảnh đó
-					var firstImage = _image.GetAllImages().FirstOrDefault(c => c.ShoesDetailsID == shoes.ShoesDetailsId);
-					if (firstImage != null)
+					if (int.TryParse(price, out int minprice))
 					{
-						shoes.ImageUrl = firstImage.Image1;
-					}
-					var nameProduct = _product.GetAllProducts().FirstOrDefault(c => c.ProductID == shoes.ProductID);
-					if (nameProduct != null)
-					{
-						ViewBag.NameSP = nameProduct.Name;
-					}
-				}
-				for (int size = 35; size <= 49; size++)
-				{
-					if (sizes.Contains(size.ToString()))
-					{
-						var sizesList = _size.GetAllSizes().Where(x => x.Name == size.ToString());
-						List<ShoesDetails> maleShoesList = comparingShoesList.Where(shoes => sizesList.Any(size => size.SizeID == shoes.SizeID)).ToList();
-						combinedShoesList.AddRange(maleShoesList);
-					}
-				}
-
-				ViewBag.shoesList = combinedShoesList;
-				return View("ListProduct");
-			}
-			return RedirectToAction("ListProduct");
-		}
-
-		public IActionResult PriceFilter()
-		{
-			return View("ListProduct");
-		}
-
-		[HttpPost]
-		public IActionResult PriceFilter(string[] minPrice)
-		{
-			var combinedShoesList = new List<ShoesDetails>();
-			var comparingShoesList = _shoesDT.GetAllShoesDetails();
-
-			foreach (var shoes in comparingShoesList)
-			{
-				// Check if the ShoesDetails has an associated image in the database
-				var firstImage = _image.GetAllImages().FirstOrDefault(c => c.ShoesDetailsID == shoes.ShoesDetailsId);
-				if (firstImage != null)
-				{
-					shoes.ImageUrl = firstImage.Image1;
-				}
-
-				// Check if the ShoesDetails has an associated product in the database
-				var nameProduct = _product.GetAllProducts().FirstOrDefault(c => c.ProductID == shoes.ProductID);
-				if (nameProduct != null)
-				{
-					ViewBag.NameSP = nameProduct.Name;
-				}
-			}
-
-			foreach (var price in minPrice)
-			{
-				if (int.TryParse(price, out int minprice))
-				{
-					if (minprice == 0)
-					{
-						var shoesList = _shoesDT.GetAllShoesDetails().Where(x => x.Price > minprice && x.Price <= 1000000);
-						combinedShoesList.AddRange(shoesList);
-					}
-					else if (minprice == 1001000)
-					{
-						var shoesList = _shoesDT.GetAllShoesDetails().Where(x => x.Price > minprice && x.Price <= 2700000);
-						combinedShoesList.AddRange(shoesList);
-					}
-					else if (minprice == 2701000)
-					{
-						var shoesList = _shoesDT.GetAllShoesDetails().Where(x => x.Price > minprice && x.Price <= 3999000);
-						combinedShoesList.AddRange(shoesList);
-					}
-					else if (minprice == 4000000)
-					{
-						var shoesList = _shoesDT.GetAllShoesDetails().Where(x => x.Price >= minprice);
-						combinedShoesList.AddRange(shoesList);
-					}
-				}
-			}
-
-			ViewBag.shoesList = combinedShoesList;
-			return View("ListProduct");
-		}
-
-		public IActionResult SupplierFilter()
-		{
-			return View("ListProduct");
-		}
-
-		[HttpPost]
-		public IActionResult SupplierFilter(string[] brands)
-		{
-			if (brands != null && brands.Length > 0)
-			{
-				var combinedShoesList = new List<ShoesDetails>();
-				var comparingShoesList = _shoesDT.GetAllShoesDetails();
-
-				foreach (var shoes in comparingShoesList)
-				{
-					// Check if the ShoesDetails has an associated image in the database
-					var firstImage = _image.GetAllImages().FirstOrDefault(c => c.ShoesDetailsID == shoes.ShoesDetailsId);
-					if (firstImage != null)
-					{
-						shoes.ImageUrl = firstImage.Image1;
-					}
-
-					// Check if the ShoesDetails has an associated product in the database
-					var nameProduct = _product.GetAllProducts().FirstOrDefault(c => c.ProductID == shoes.ProductID);
-					if (nameProduct != null)
-					{
-						ViewBag.NameSP = nameProduct.Name;
+						if (minprice == 0)
+						{
+							var shoesList = _shoesDT.GetAllShoesDetails().Where(x => x.Price > minprice && x.Price <= 1000000).ToList();
+							AddRangeIfNotExist(combinedShoesList, shoesList);
+						}
+						else if (minprice == 1001000)
+						{
+							var shoesList = _shoesDT.GetAllShoesDetails().Where(x => x.Price > minprice && x.Price <= 2700000).ToList();
+							AddRangeIfNotExist(combinedShoesList, shoesList);
+						}
+						else if (minprice == 2701000)
+						{
+							var shoesList = _shoesDT.GetAllShoesDetails().Where(x => x.Price > minprice && x.Price <= 3999000).ToList();
+							AddRangeIfNotExist(combinedShoesList, shoesList);
+						}
+						else if (minprice == 4000000)
+						{
+							var shoesList = _shoesDT.GetAllShoesDetails().Where(x => x.Price >= minprice).ToList();
+							AddRangeIfNotExist(combinedShoesList, shoesList);
+						}
 					}
 				}
 
-				var brandList = _supplier.GetAllSuppliers().Where(suppliers => brands.Contains(suppliers.Name.ToLower()));
-				var filteredShoesList = comparingShoesList.Where(shoes => brandList.Any(brand => brand.SupplierID == shoes.SupplierID));
-				combinedShoesList.AddRange(filteredShoesList);
+				//Filter hãng
+				var brandList = _supplier.GetAllSuppliers().Where(suppliers => brands.Contains(suppliers.Name.ToLower())).ToList();
+				var filteredBrandsList = comparingShoesList.Where(shoes => brandList.Any(brand => brand.SupplierID == shoes.SupplierID)).ToList();
+				AddRangeIfNotExist(combinedShoesList, (List<ShoesDetails>)filteredBrandsList);
 
+				//Filter style
+				var stylesList = _style.GetAllStyles().Where(s => styles.Contains(s.Name.ToLower())).ToList();
+				var filteredStylesList = comparingShoesList.Where(shoes => stylesList.Any(style => style.StyleID == shoes.StyleID)).ToList();
+				AddRangeIfNotExist(combinedShoesList, (List<ShoesDetails>)filteredStylesList);
+
+				//Filter màu sắc
+				var colorsList = _color.GetAllColors().Where(s => colors.Contains(s.Name.ToLower())).ToList();
+				var filteredColorsList = comparingShoesList.Where(shoes => colorsList.Any(color => color.ColorID == shoes.ColorID)).ToList();
+				AddRangeIfNotExist(combinedShoesList, (List<ShoesDetails>)filteredColorsList);
+
+				//Tổng hợp lịa thành 1 list và gửi lên ViewBag
 				ViewBag.shoesList = combinedShoesList;
 				return View("ListProduct");
 			}
 			return RedirectToAction("ListProduct");
 		}
 
-		public IActionResult StyleFilter()
+		//Lọc xem sản phẩm đã có trong combinedShoesList chưa, nếu chưa thì thêm vào
+		private void AddRangeIfNotExist(List<ShoesDetails> targetList, List<ShoesDetails> sourceList)
 		{
-			return View("ListProduct");
-		}
-
-
-		[HttpPost]
-		public IActionResult StyleFilter(string[] styles)
-		{
-			if (styles != null && styles.Length > 0)
+			foreach (var item in sourceList)
 			{
-				var combinedShoesList = new List<ShoesDetails>();
-				var comparingShoesList = _shoesDT.GetAllShoesDetails();
-
-				foreach (var shoes in comparingShoesList)
+				if (!targetList.Contains(item))
 				{
-					// Check if the ShoesDetails has an associated image in the database
-					var firstImage = _image.GetAllImages().FirstOrDefault(c => c.ShoesDetailsID == shoes.ShoesDetailsId);
-					if (firstImage != null)
-					{
-						shoes.ImageUrl = firstImage.Image1;
-					}
-
-					// Check if the ShoesDetails has an associated product in the database
-					var nameProduct = _product.GetAllProducts().FirstOrDefault(c => c.ProductID == shoes.ProductID);
-					if (nameProduct != null)
-					{
-						ViewBag.NameSP = nameProduct.Name;
-					}
+					targetList.Add(item);
 				}
-
-				var stylesList = _style.GetAllStyles().Where(s => styles.Contains(s.Name.ToLower()));
-				var filteredShoesList = comparingShoesList.Where(shoes => stylesList.Any(style => style.StyleID == shoes.StyleID));
-				combinedShoesList.AddRange(filteredShoesList);
-				ViewBag.shoesList = combinedShoesList;
-				return View("ListProduct");
 			}
-			return RedirectToAction("ListProduct");
 		}
-
-		public IActionResult ColorFilter()
-		{
-			return View("ListProduct");
-		}
-
-		[HttpPost]
-		public IActionResult ColorFilter(string[] colors)
-		{
-			if (colors != null || colors.Length > 0)
-			{
-				var combinedShoesList = new List<ShoesDetails>();
-				var comparingShoesList = _shoesDT.GetAllShoesDetails();
-
-				foreach (var shoes in comparingShoesList)
-				{
-					// Check if the ShoesDetails has an associated image in the database
-					var firstImage = _image.GetAllImages().FirstOrDefault(c => c.ShoesDetailsID == shoes.ShoesDetailsId);
-					if (firstImage != null)
-					{
-						shoes.ImageUrl = firstImage.Image1;
-					}
-
-					// Check if the ShoesDetails has an associated product in the database
-					var nameProduct = _product.GetAllProducts().FirstOrDefault(c => c.ProductID == shoes.ProductID);
-					if (nameProduct != null)
-					{
-						ViewBag.NameSP = nameProduct.Name;
-					}
-				}
-
-				var colorsList = _color.GetAllColors().Where(s => colors.Contains(s.Name.ToLower()));
-				var filteredShoesList = comparingShoesList.Where(shoes => colorsList.Any(color => color.ColorID == shoes.ColorID));
-				combinedShoesList.AddRange(filteredShoesList);
-				ViewBag.shoesList = combinedShoesList;
-				return View("ListProduct");
-			}
-			return RedirectToAction("ListProduct");
-		}
-
 	}
 }
